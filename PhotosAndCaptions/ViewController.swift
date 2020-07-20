a948f05a00199d71f2f327eccf31e452658fad56
(null) <heldersrvio@gmail.com>
2020-07-20 10:39:13 -0300

    Initial Commit

---
+++ PhotosAndCaptions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosAndCaptions
+//
+//  Created by Helder on 20/07/20.
+//  Copyright © 2020 Helder de Melo Sérvio Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
